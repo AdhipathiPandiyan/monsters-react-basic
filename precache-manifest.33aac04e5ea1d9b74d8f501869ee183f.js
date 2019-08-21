2551322950dc199aac44977b1c125e9ed5fee6b8
Adhipathi Pandiyan <adhipathi12@gmail.com>
2019-08-21 19:45:14 +0530

    Updates

---
+++ precache-manifest.33aac04e5ea1d9b74d8f501869ee183f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921db5c3094c7115b49d5002df7c1b63",
+    "url": "/monsters-react-basic/index.html"
+  },
+  {
+    "revision": "01e4329fc8ad72b17fd9",
+    "url": "/monsters-react-basic/static/css/main.2f95af91.chunk.css"
+  },
+  {
+    "revision": "f7e71d2cca931e5dd749",
+    "url": "/monsters-react-basic/static/js/2.89e16e46.chunk.js"
+  },
+  {
+    "revision": "01e4329fc8ad72b17fd9",
+    "url": "/monsters-react-basic/static/js/main.52a62c7d.chunk.js"
+  },
+  {
+    "revision": "efbf8103327d8fe91df6",
+    "url": "/monsters-react-basic/static/js/runtime~main.fceb87c4.js"
+  }
+]);
